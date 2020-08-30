6e72156ffe9718ddc017895fc3ee1056b8220753
Dan Gillespie <daniel.gillespie0949@gmail.com>
2020-08-30 10:28:17 -0700

    added docs on address space

--- Memory/MemoryBus.cs
+++ Memory/MemoryBus.cs
@@ -18,24 +18,42 @@ namespace CoreBoy
             if (address < 0x8000)
             {
                 // Cartridge ROM
+                // 0 - 7FFF
+                // 32kb
+
                 // TODO
                 return 0;
             }
             else if (address < 0xA000)
             {
-                // VRAM Banking
+                // Video Ram (Banking)
+                // 8000 - 9FFFF
+                // 8kb
+
                 // TODO
                 return 0;
             }
             else if (address < 0xC000)
             {
-                // Cartridge ROM
+                // External RAM (Cartridge RAM / contains game save data, etc)
+                // A000 - BFFF
+                // 8kb
+
                 // TODO
                 return 0;
             }
             else if (address < 0xD000)
             {
-                // Internal RAM Banks (1-7)
+                // Internal Work RAM (Bank 0)
+                // C000 - DFFF
+                // 8kb
+                // TODO
+                return 0;
+            }
+            else if (address < 0xE000)
+            {
+                // Internal Work RAM (Bank 1-7)
+                
                 // TODO
                 return 0;
             }
@@ -47,71 +65,78 @@ namespace CoreBoy
             }
             else if(address < 0xFEA0)
             {
-                // Object Attribute Memory (OAM) Table
+                // Object Attribute Memory (OAM) / Sprite Attribute Table
+                // FE00 - FE9F
+                // 160 bytes
+                // Each sprite takes up 4 bytes which leaves just enough room for exactly 40 sprites
+
                 // TODO
                 return 0;
             }
             else if (address < 0xFF00)
             {
+                // Talk directly to screen controller, sound generator, buttons, link cable, and internal timers
                 // Unusuable memory
+                // 127 bytes
                 return 0xFF;
             }
             else
             {
-                // If get here assume reading from High Memory
+                // High RAM (in Gameboy hardware this RAM sits inside the CPU and is faster than Work RAM)
+                // FF80 - FFFE
+                // 127 bytes
+
                 // TODO
                 return 0;
             }
+
+            // FFFF
+            // Interrupt switch
+            // Unusable memory
+            // 1 byte
         }
 
         public void Write(UInt16 address, byte value)
         {
             if (address < 0x8000)
             {
-                // Cartridge ROM (banking)
+                // Cartridge ROM
+                // 0 - 7FFF
+                // 32kb
                 // TODO
-              
             }
             else if (address < 0xA000)
             {
-                // VRAM Banking
+                // Video Ram (Banking)
                 // TODO
-               
             }
             else if (address < 0xC000)
             {
                 // Cartridge ROM
                 // TODO
-             
             }
             else if (address < 0xD000)
             {
                 // Internal RAM Banks (1-7)
-                // TODO
-              
             }
             else if (address < 0xFE00)
             {
                 // Echo RAM
                 // TODO?
-               
             }
             else if (address < 0xFEA0)
             {
                 // Object Attribute Memory (OAM) Table
                 // TODO
-              
             }
             else if (address < 0xFF00)
             {
                 // Unusuable memory
-               
             }
             else
             {
                 // If get here assume reading from High Memory
                 // TODO
-              
             }
         }
     }
--- README.md
+++ README.md
@@ -13,3 +13,7 @@ TODO
 * https://www.youtube.com/watch?v=RZUDEaLa5Nw
 
 * https://www.youtube.com/watch?v=ecTQVa42sJc
+
+* https://gbdev.io/pandocs/
+
+* https://gekkio.fi/files/gb-docs/gbctr.pdf
